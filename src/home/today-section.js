b409b48b4a3576c95104c739df3e7b0f4fb4d723
Jaehong Kim <jjaekong@cnspartner.com>
2024-01-12 16:12:59 +0900

    today ui

--- src/home/today-section.js
+++ src/home/today-section.js
@@ -1,20 +1,20 @@
 import { html } from "lit-html";
 
-const milongaEventList = [0,0,0]
+const milongaEventList = [0, 0, 0]
 
 const milongaEventItem = () => html`
     <a href="milonga.html" class="flex w-100 items-center">
-        <div>
+        <div class="self-start">
             <time class="flex flex-col rounded-xl justify-center items-center leading-tight size-14 bg-slate-100">
                 <span class="font-bold">8</span>
                 <span class="text-slate-400 text-xs">PM</span>
             </time>
         </div>
-        <div class="mx-3 self-start">
+        <div class="mx-3">
             <h6 class="text-md font-extrabold">루미노소</h6>
             <div class="text-xs text-slate-400">@오나다</div>
         </div>
-        <div class="ms-auto">
+        <div class="ms-auto self-start">
             <img class="block size-14 rounded-xl" src="https://picsum.photos/id/10/100/100">
         </div>
     </a>
@@ -24,7 +24,7 @@ export const todaySection = () => {
     return html`
         <section class="rounded-[1rem] bg-white shadow-lg shadow-slate-100 p-5">
             <header>
-                <h2 class="text-2xl font-extrabold">오늘</h2>
+                <h2 class="text-2xl font-black">오늘</h2>
             </header>
             <ul>
                 ${ milongaEventList.map(() => html`

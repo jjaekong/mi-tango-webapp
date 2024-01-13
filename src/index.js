418682323ab3d8cafdea1bbe42a4a8d0082c62d2
jjaekong <jjaekong@gmail.com>
2024-01-13 18:53:19 +0900

    스켈레톤 UI 시험 적용

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import './firebase.js'
 import { html, render } from 'lit-html'
 import { todaySection } from './home/today-section.js'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
-import { userCircleOutline } from './icons.js'
+
+// render(todaySection(), document.getElementById("main"))
 
 onAuthStateChanged(getAuth(), user => {
+    
     if (user) {
         render(html`
             <a href="me.html"><img class="block size-8 rounded-full" src="${user.photoURL}"></a>
-        `, document.getElementById("profile"))
+        `, document.getElementById("user-profile"))
     } else {
         render(html`
             <a href="login.html" class="text-2xl font-bold text-slate-700">
@@ -16,8 +18,37 @@ onAuthStateChanged(getAuth(), user => {
                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 </svg>
             </a>
-        `, document.getElementById("profile"))
+        `, document.getElementById("user-profile"))
+
     }
 })
 
-render(todaySection(), document.getElementById("main"))
+const milongaEventList = [10, 100, 1000, 1050, 550]
+
+const milongaEventItem = (item) => {
+    return html`
+        <a href="milonga.html" class="flex w-100 items-center">
+            <div class="self-start">
+                <time class="flex flex-col rounded-xl justify-center items-center leading-tight size-14 bg-slate-100">
+                    <span class="font-bold">8</span>
+                    <span class="text-slate-400 text-xs">PM</span>
+                </time>
+            </div>
+            <div class="mx-3">
+                <h6 class="text-md font-extrabold">루미노소</h6>
+                <div class="text-xs text-slate-400">@오나다</div>
+            </div>
+            <div class="ms-auto self-start">
+                <img class="block size-14 rounded-xl" src="https://picsum.photos/id/${item}/100/100">
+            </div>
+        </a>
+`}
+
+setTimeout(() => {
+    document.querySelector('#today-milongas ul').innerHTML = ''
+    render(html`${
+        milongaEventList.map((item) => html`
+            <li class="mt-3">${milongaEventItem(item)}</li>
+        `)
+    }`, document.querySelector('#today-milongas ul'))
+}, 1000)

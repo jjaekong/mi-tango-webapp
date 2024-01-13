be9f631f200c3f5de9eb503ddaff69aefbdc01b1
jjaekong <jjaekong@gmail.com>
2024-01-13 19:11:51 +0900

    디제이 추가

--- public/js/index.js
+++ public/js/index.js
@@ -11151,8 +11151,19 @@ const milongaEventItem = (item) => {
                 </time>
             </div>
             <div class="mx-3">
-                <h6 class="text-md font-extrabold">루미노소</h6>
-                <div class="text-xs text-slate-400">@오나다</div>
+                <h6 class="font-extrabold">루미노소</h6>
+                <div class="text-xs text-slate-400 flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25" />
+                    </svg>
+                    <span>오나다</span>
+                </div>
+                <div class="text-xs text-slate-400 flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" class="h-4 w-4">
+                        <path d="M8 3a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V8a6 6 0 1 1 12 0v5a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h1V8a5 5 0 0 0-5-5"/>
+                    </svg>
+                    <span>시스루</span>
+                </div>
             </div>
             <div class="ms-auto self-start">
                 <img class="block size-14 rounded-xl" src="https://picsum.photos/id/${item}/100/100">
@@ -11167,4 +11178,4 @@ setTimeout(() => {
             <li class="mt-3">${milongaEventItem(item)}</li>
         `)
     }`, document.querySelector('#today-milongas ul'));
-}, 1000);
+}, 300);

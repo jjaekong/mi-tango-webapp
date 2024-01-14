d98628811ff60ae51e6f0fdfafacfb5876e45cf6
jjaekong <jjaekong@gmail.com>
2024-01-14 13:14:51 +0900

    me 작성 중...

--- src/me.js
+++ src/me.js
@@ -1,5 +1,25 @@
-import { getAuth, signOut } from 'firebase/auth'
+import { html, render } from 'lit-html'
 import './firebase.js'
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth'
+
+const setProfile = (user) => {
+    if (!user) return
+    render(html`<img class="rounded-full" src=${user.photoURL}>`, document.querySelector('#user-photo'))
+    render(html`${user.displayName}`, document.querySelector('#user-name'))
+    render(html`${user.email}`, document.querySelector('#user-email'))
+}
+
+const currentUser = getAuth().currentUser
+
+if (currentUser) setProfile(currentUser)
+
+onAuthStateChanged(getAuth(), user => {
+    if (user) {
+        setProfile(user)
+    } else {
+        location.replace('/')
+    }
+})
 
 document.querySelector('#logout')?.addEventListener('click', function() {
     if (confirm("로그아웃 하시겠습니까?")) {

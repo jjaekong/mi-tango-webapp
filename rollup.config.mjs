07b7a340b4ee895fd7ee263b1e4c7f37c1ed21d9
Jaehong Kim <jjaekong@gmail.com>
2024-01-25 15:46:01 +0900

    react test

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,28 @@
 import resolve from '@rollup/plugin-node-resolve';
+import babel, { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 
 export default {
 	input: './src/index.js',
 	output: {
-		dir: "./public/",
-		format: 'esm'
+		file: './public/index.js',
+		format: 'esm',
+		plugins: [getBabelOutputPlugin({ presets: ['@babel/preset-env'] })],
 	},
-	plugins: [resolve()]
+	plugins: [
+		resolve({
+			extensions: ['.js', '.jsx']
+		}),
+		babel({
+			babelHelpers: 'bundled',
+			presets: ['@babel/preset-react'],
+			extensions: ['js', '.jsx']
+		}),
+		commonjs(),
+		replace({
+			'preventAssignment': false,
+			'process.env.NODE_ENV': '"development"'
+		})
+	]
 }
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Counter = () => <div>coutner</div>
+
+export default function App() {
+	return (
+		<>
+			<Counter></Counter>
+		</>
+	)
+}

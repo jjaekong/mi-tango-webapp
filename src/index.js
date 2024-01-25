5d16eb10b5473b97dcef020900e30edfe146742c
jjaekong <jjaekong@gmail.com>
2024-01-26 06:40:51 +0900

    화면 구성 중

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import { html, render } from 'lit-html'
 import { homeTemplate } from './home'
 
-render(homeTemplate(), document.getElementById('app'))
+const appEl = document.getElementById('app')
+render(html`home`, appEl)
+
+const setScreenByHash = () => {
+    if (location.hash === '') {
+        render(html`home`, appEl)
+    } else if (location.hash === '#milonga') {
+        render(html`milonga`, appEl)
+    }
+}
+
+window.addEventListener('DOMContentLoaded', e => {
+    setScreenByHash()
+})
+
+window.addEventListener("hashchange", e => {
+    setScreenByHash()
+}, false)

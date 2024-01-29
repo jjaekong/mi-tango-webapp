13c5fb22135e695f47cff9a52c2e89cd26df714c
jjaekong <jjaekong@gmail.com>
2024-01-29 22:36:55 +0900

    me my milongas ui

--- public/index.js
+++ public/index.js
@@ -26642,12 +26642,26 @@ const NewMilonga = async () => {
 
 const Milonga = async () => {
 
-	const auth = getAuth();
+	// const auth = getAuth()
 
-	await auth.authStateReady();
+	// await auth.authStateReady()
+
+    const milongaId = location.hash.split('/')[1];
+
+    console.log('milonga id ', milongaId);
+
+    const db = getFirestore();
+    const milongaRef = doc(db, `${ENV}.milongas`, milongaId);
+    const milongaSnap = await getDoc(milongaRef);
+
+    if (!milongaSnap.exists()) {
+        location.replace('#');
+    }
+
+    const milongaData = milongaSnap.data();
 
 	j$1(h(x$1`
-		milonga
+		${milongaData.name}
 	`), document.getElementById('app'));
 };
 
--- src/milonga.js
+++ src/milonga.js
@@ -1,14 +1,30 @@
 import { getAuth } from 'firebase/auth'
+import { doc, getDoc, getFirestore } from 'firebase/firestore'
 import { render, html } from 'lit-html'
 import { cache } from 'lit-html/directives/cache.js'
+import { ENV } from './config'
 
 export const Milonga = async () => {
 
-	const auth = getAuth()
+	// const auth = getAuth()
 
-	await auth.authStateReady()
+	// await auth.authStateReady()
+
+    const milongaId = location.hash.split('/')[1]
+
+    console.log('milonga id ', milongaId)
+
+    const db = getFirestore()
+    const milongaRef = doc(db, `${ENV}.milongas`, milongaId)
+    const milongaSnap = await getDoc(milongaRef)
+
+    if (!milongaSnap.exists()) {
+        location.replace('#')
+    }
+
+    const milongaData = milongaSnap.data()
 
 	render(cache(html`
-		milonga
+		${milongaData.name}
 	`), document.getElementById('app'))
 }

6ed98893a2f35e2282dceb18822ad660d45d42fc
Jaehong Kim <jjaekong@gmail.com>
2024-02-01 15:48:06 +0900

    ui

--- public/index.js
+++ public/index.js
@@ -26736,21 +26736,21 @@ const Milonga = async () => {
 			</header>
             <div class="milonga-profile flex mb-4">
                 <div class="flex-none">
-                    <img src="https://picsum.photos/100/100" class="block size-24 rounded-xl">
+                    <img src="https://picsum.photos/100/100" class="block size-24 rounded-2xl">
                 </div>
                 <div class="mx-3 flex-1">
 					<h4 class="font-bold">${milongaData.name}</h4>
 					<span class="text-slate-500 text-sm flex items-center">${AtSymbolIcon({ 'classList': 'size-4 me-1'})} ${milongaId}</span>
 				</div>
             </div>
-			<a href="#add_milonga_event?mid=${milongaId}" class="block p-3 bg-purple-500 text-white rounded-lg text-center mb-4 font-bold">밀롱가 이벤트 추가</a>
             <section class="p-5 mb-4 rounded-xl bg-white shadow-xl shadow-slate-100">
-                <header class="mb-4">
-                    <h4 class="font-bold">다가오는 밀롱가 이벤트</h4>
+                <header class="mb-5 flex items-center justify-between">
+                    <h4 class="font-bold text-lg">다가오는 이벤트</h4>
+					<a href="#add_milonga_event?mid=${milongaId}" class="text-purple-500 font-bold">이벤트 추가</a>
                 </header>
 				<ul>
 					${
-						o([10, 100], item => x$1`<li class="mt-3">${MilongaEventItem(item)}</li>`)
+						o([10], item => x$1`<li class="mt-3">${MilongaEventItem(item)}</li>`)
 					}
 				</ul>
             </section>
